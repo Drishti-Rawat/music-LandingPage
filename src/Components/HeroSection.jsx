0eea2cd197ac33e19bf345f28cab6e5692dc2e3d
Drishti <drishtirwt019@gmail.com>
2024-10-28 23:48:30 +0530

    .

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         fill="blue"/>
 <div className="p-4 relative z-10 w-full text-center flex flex-col">
       <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the Art of Music</h1>
-      <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Dive into Our comprhensive music courses and transform our musical journey today. whether you're a begginer or looking to rfine your skills,join us to unlock your true potential. </p>
+      <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Dive into Our comprhensive music courses and transform our musical journey today. whether you&apos;re a begginer or looking to rfine your skills,join us to unlock your true potential. </p>
 </div>
 
 <div className="mt-4">
